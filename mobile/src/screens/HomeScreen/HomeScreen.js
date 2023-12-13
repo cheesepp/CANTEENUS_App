e8db7ee533df0f1e26bf04eb021d9d162c9cc33d
cheesepp <nhattri2904@gmail.com>
2023-12-13 10:41:34 +0700

    tạo màn hình homepage

--- mobile/src/screens/HomeScreen/HomeScreen.js
+++ mobile/src/screens/HomeScreen/HomeScreen.js
@@ -1,12 +1,57 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import {Text, TouchableOpacity, View} from 'react-native';
+import styles from './styles';
+
+
+// import * as React from 'react';
+// import { View, Text } from 'react-native';
+
+// export default function ChatScreen({ navigation }) {
+//     return (
+//         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//             <Text
+//                 onPress={() => alert('This is the "Home" screen.')}
+//                 style={{ fontSize: 26, fontWeight: 'bold' }}>ChatScreen Screen</Text>
+//         </View>
+//     );
+// }
 
 export default function HomeScreen({ navigation }) {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text
-                onPress={() => alert('This is the "Home" screen.')}
-                style={{ fontSize: 26, fontWeight: 'bold' }}>Home Screen</Text>
+    const handleButtonPress = (buttonNumber) => {
+        // Handle button press based on buttonNumber
+        console.log(`Button ${buttonNumber} pressed`);
+      };
+    
+      return (
+        <View style={styles.container}>
+          <View style={styles.row}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => handleButtonPress(1)}
+            >
+              <Text style={styles.buttonText}>Nhân viên</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => handleButtonPress(2)}
+            >
+              <Text style={styles.buttonText}>Menu</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.row}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => handleButtonPress(3)}
+            >
+              <Text style={styles.buttonText}>Kho</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => handleButtonPress(4)}
+            >
+              <Text style={styles.buttonText}>Chỉ tiêu</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-    );
+      );
 }
--- mobile/src/screens/HomeScreen/styles.js
+++ mobile/src/screens/HomeScreen/styles.js
@@ -1,12 +1,34 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-  Wrapper: {
-    backgroundColor: "#FFFFFF",
+  container: {
     flex: 1,
-    padding: 40,
-    width: "100%",
+    justifyContent: 'top',
+    alignItems: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+    // marginBottom: 10,
+    // justifyContent: 'space-between',
+    // padding: 10,
+  },
+  button: {
+    backgroundColor: '#3498db',
+    margin: 10,
+    // borderRadius: 30,
+    // marginLeft: ,
+    borderRadius: 20,
+    height: 135,
+    width: 164,
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: '#ffffff',
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
+
 
 export default styles;
