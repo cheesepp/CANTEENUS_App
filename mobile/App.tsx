2f7e987f5bf94d6a62576f2b3f08b8c6102df578
cheesepp <nhattri2904@gmail.com>
2023-12-16 17:43:44 +0700

    merged code - mainv1

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,13 +2,29 @@ import MainContainer from './src/routes/MainContainer';
 import { Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet, View } from 'react-native'
+import LoginScreen from './src/screens/LoginScreen/LoginScreen'
+import ForgetPasswordScreen from './src/screens/ForgetPasswordScreen/ForgetPasswordScreen'
 
+//File App.tsx chính của ứng dụng mobile
+//Ở đây chỉ cần gọi màn hình LoginScreen
 
 function App() {
-  const Stack = createNativeStackNavigator();
   return (
-    <MainContainer/>
-  );
+    <View >
+      <LoginScreen/>
+      {/* <ForgetPasswordScreen/> */}
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({})
+
+// function App() {
+//   const Stack = createNativeStackNavigator();
+//   return (
+//     <MainContainer/>
+//   );
+// }
 
 export default App;
